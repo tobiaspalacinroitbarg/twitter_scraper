31b7d4680be5f54a1abc4d45ae1123a8ec659c9c
Lorenzo Kw <114435037+lolookw@users.noreply.github.com>
2025-01-05 17:48:42 -0300

    Add files via upload
    Agregué archivo .json y sus manejos, comenté algunas funciones que ya no se usan (igual queda por limpiar parte del código, no borré nada por las dudas). Corregí un error en users_check() que a veces se rompía con ciertos números sin "." y con "K".

--- funcs.py
+++ funcs.py
@@ -8,8 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-import urllib.parse
+from urllib.parse import quote
 import os
+import json
 
 def send_messages(driver):
     with open ("./checked_users_list.txt") as f:
@@ -32,15 +33,16 @@ def send_messages(driver):
         print(f"ERROR en el usuario {user}")
     return
 
-def get_users(urls, username, password):
+def get_users(busquedas, username, password):
     if not os.path.exists('./backups'):
         os.makedirs('./backups')    
     final_list = []
-    for index, url in enumerate(urls):
+    for busqueda in busquedas:
+        scrl_post, scrl_comment, url = busqueda[0], busqueda[1], busqueda[2]
         driver = login(username, password)
         driver.get(url)
         time.sleep(3)
-        temp_list = get_data(driver, url[23:28])
+        temp_list = get_data(driver, url[23:28], scrl_post, scrl_comment)
         temp_list = list(set(temp_list))
         final_list.extend(temp_list)
         with open(f"./backups/{url[23:28]}/final_{url[23:28]}.txt", "w") as f: 
@@ -56,18 +58,18 @@ def get_users(urls, username, password):
     driver.quit()
     return driver
 
-def get_data(driver, busqueda):
+def get_data(driver, busqueda, scrl_post, scrl_comment):
     if not os.path.exists(f'./backups/{busqueda}'):
         os.makedirs(f'./backups/{busqueda}')
     users_list = []
-    driver, posts_url = scroll_down_get(driver, iter=5)
+    driver, posts_url = scroll_down_get(driver, iter=scrl_post)
     posts_url = list(set(posts_url))
     print(f"POST URLS:{len(posts_url)}, {posts_url}")
     for index, post_url in enumerate(posts_url):
         users_list.extend([post_url.split("/")[3]])
         driver.get(post_url)
         time.sleep(5)
-        driver, comment_urls = scroll_down_get(driver=driver, iter=2)
+        driver, comment_urls = scroll_down_get(driver=driver, iter=scrl_comment)
         time.sleep(5)
         print(f"COMMENT URLS:{len(comment_urls)}, {comment_urls}")
         users_list.extend([url.split("/")[3] for url in comment_urls])
@@ -82,7 +84,7 @@ def scroll_down_get(driver: webdriver, iter):
     posts_list = []
     count_error=0
     feed = element_exists(driver=driver, by=By.XPATH,ref='//div[@aria-label="Home timeline"]',time=20)
-    for index in range(0,iter):
+    for _ in range(iter):
         try:
             feed.send_keys(Keys.PAGE_DOWN)
             feed.send_keys(Keys.PAGE_DOWN)
@@ -113,13 +115,13 @@ def element_exists(driver:webdriver, by:By, ref:str, time=4, refresh=False):
 
 def get_urls():
     urls= []
-    with open ("./busquedas.txt") as f:
-        lines = f.readlines()
-        busquedas = [line for line in lines]
-    for busqueda in busquedas:
-        if len(busqueda)>1:
-            url = "https://x.com/search?q=" + urllib.parse.quote(busqueda) + "&src=typed_query&f=live"
-            urls.append(url)
+    """with open ("./busquedas.txt") as f:
+            lines = f.readlines()
+            busquedas = [line for line in lines]
+        for busqueda in busquedas:
+            if len(busqueda)>1:
+                url = "https://x.com/search?q=" + urllib.parse.quote(busqueda) + "&src=typed_query&f=live"
+                urls.append(url) """
     return urls
 
 def filter_users(driver):
@@ -127,7 +129,7 @@ def filter_users(driver):
         with open ("./users_list.txt") as f:
             lines = f.readlines()
             users = [line.strip() for line in lines]
-        for user in users:
+        for index, user in enumerate(users):
             driver.get(f"https://x.com/{user}")
             time.sleep(2)
             followers_count_raw = element_exists(driver, By.XPATH, f"//a[@href='/{user}/verified_followers']")
@@ -137,7 +139,7 @@ def filter_users(driver):
                     print(f"USER: {user} no pasó los requisitos")
                     continue
                 if("K" in followers_count):
-                    if int(followers_count.strip("K").split(".")[0])<35:
+                    if int(followers_count.split(" ")[0].split(".")[0].strip("K"))<35:
                         checked_users.append(user)
                     else:
                         print(f"USER: {user} no pasó los requisitos")
@@ -147,6 +149,8 @@ def filter_users(driver):
             else: 
                 print(f"USER: {user} no pasó los requisitos")
                 continue
+            if index%20==0:
+                print(checked_users)
         with open(f"./checked_users_list.txt", "w") as f:
             for user in checked_users:
                 f.write(f"{user}\n") 
@@ -169,19 +173,20 @@ def login(username, password):
     
     time.sleep(5)
     return driver
-    chrome_options = ChromeOptions()
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-    driver = webdriver.Chrome(options=chrome_options)
 
-    driver.get("https://x.com/i/flow/login")
-    we_email=element_exists(driver=driver,by=By.XPATH, ref='//input[@autocomplete="username"]',time=20)
-    we_email.send_keys(username)
-    we_email.send_keys(Keys.ENTER)
-
-    we_password = element_exists(driver=driver, by=By.XPATH,ref='//input[@name="password"]',time=20)
-    we_password.send_keys(password)
-    we_password.send_keys(Keys.ENTER)
+def load_config(filepath="config.json"):
+    with open(filepath, "r") as f:
+        config_file = json.load(f)
     
-    time.sleep(5)
-    return driver
+    username = config_file["username"]
+    password = config_file["password"]
+    busquedas = [
+        [
+            b["scroll_posts"],
+            b["scroll_comments"],
+            f"https://x.com/search?q={quote(b['query'])}&src=typed_query&f=live"
+        ]
+        for b in config_file["busquedas"]
+    ]
+
+    return username, password, busquedas
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from funcs import get_urls, get_users,send_messages,login, filter_users 
-
-USERNAME ="NftScpr"
-PASSWORD = "scrapperbot25"
+from funcs import get_urls, get_users,send_messages,login, filter_users, load_config
+from urllib.parse import quote
 
 if __name__=="__main__":
-    urls = get_urls()
-    driver = get_users(urls, USERNAME, PASSWORD)
+    username, password, busquedas = load_config()
+    print(busquedas)
+    #urls = get_urls()
+    #driver = get_users(busquedas, username, password)
+    driver = login(username, password)
     filter_users(driver)
     #send_messages(driver)
     
